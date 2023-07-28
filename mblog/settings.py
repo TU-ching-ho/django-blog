9da2d88d8e58efcc95e7857239377079866f7053
mark <gh850702@gmail.com>
2023-07-28 18:00:37 +0800

    製作首頁以及文章分頁

--- mblog/settings.py
+++ mblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*-d_)54z)0ixl^f#uq^765amu*10=7j+7qc_ms+lao0%%mx1cz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "mysite",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "mblog.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "zh-hant"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Taipei"
 
 USE_I18N = True
 
--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from mysite.models import Post
+from datetime import datetime
 # Create your views here.
+
+
+def homepage(request):
+    #posts = Post.objects.all()
+    posts = Post.objects.all().order_by("id")
+    now = datetime.now()
+    '''
+    post_lists = list()
+    for count, post in enumerate(posts):
+        # post_lists.append("No.{}:".format(str(count))+str(post)+"<br>")
+        post_lists.append("No.{}:{}".format(str(count+1), str(post))+"<br>")
+    '''
+    return render(request, "index.html", locals())
+
+
+def showpost(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            return render(request, 'post.html', locals())
+    except:
+        return redirect('/')
